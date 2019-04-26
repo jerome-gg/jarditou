5ddcb2a303ee753797417bb2052eea28bd96e5ac
jerome <developpeur.jerome@gmail.com>
2019-04-26 11:33:37 +0200

    Ajout du plugging pour l'arborescence + liens dans le header et le footer.

--- application/views/header.php
+++ application/views/header.php
@@ -4,6 +4,7 @@
     <title>Liste des produits</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="<?php echo base_url("assets/css/style.css");?>">
+    <link href = "<?php  base_url("/assets/treeview/src/css/bootstrap-treeview.css")?>"  rel = "stylesheet ">
 </head>
     <body>
         <header>
